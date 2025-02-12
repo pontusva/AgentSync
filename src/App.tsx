e209652178d397fd8af33c8f18c741dd6eca9ac1
Pontus Abrahamsson <ponabr123@gmail.com>
2025-02-12 19:02:45 +0100

    Add test error logging in App component with Logtail

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Dashboard from "@/components/dashboard";
-
+import { logtail } from "./logtail/logtail";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import { TestError } from "./components/TestError";
 
 function App() {
+  logtail.error("TEST_ERROR", {
+    dt: new Date().toISOString(),
+    source: "App",
+    type: "test_error",
+  });
+  logtail.flush();
   return (
     <ErrorBoundary>
       <div className="container">
--- src/logtail/logtail.ts
+++ src/logtail/logtail.ts
@@ -2,5 +2,4 @@ import { Logtail } from "@logtail/browser";
 
 export const logtail = new Logtail("BCfp6fLZELzGDafN2QrBK36p", {
   endpoint: "https://s1202642.eu-nbg-2.betterstackdata.com",
-  ignoreExceptions: true,
 });
